26390c46331f67c2bb50e442c45e334d547e6c46
Sinh <luuvansinh555@gmail.com>
2022-09-19 14:34:55 +0700

    add webhook util for onpoint

--- util/pjson/unmarshal.go
+++ util/pjson/unmarshal.go
@@ -2,18 +2,14 @@ package pjson
 
 import (
 	"encoding/json"
-
-	"github.com/Selly-Modules/logger"
+	"log"
 )
 
 // Unmarshal ...
 func Unmarshal(b []byte, resultP interface{}) error {
 	err := json.Unmarshal(b, resultP)
 	if err != nil {
-		logger.Error("pjson.Unmarshal", logger.LogData{
-			"raw": string(b),
-			"err": err.Error(),
-		})
+		log.Printf("3pl/util/pjson.Unmarshal: err %v, payload %s", err, string(b))
 	}
 	return err
 }
